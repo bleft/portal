1faa62b2a993d67269b8660915a3df2f91219f79
Sven Bautz <bautz@icloud.com>
2015-05-22 20:29:39 +0200

    add missing file

---
+++ php/ChatEntry.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sven.bautz
+ * Date: 22.05.15
+ * Time: 14:08
+ */
+
+class ChatEntry {
+    public $id;
+    public $AUTOR;
+    public $TIMESTAMP;
+    public $TEXT;
+
+} 
